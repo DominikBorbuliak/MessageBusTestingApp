b7a38e5edbad628c27633150fa4268c1d3c80075
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2023-02-13 22:36:32 +0100

    Implemented simulation of error for send and reply in NServiceBus

---
+++ Services/Models/ExceptionResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Services.Models
+{
+	/// <summary>
+	/// Class that represents response message returned if exception occured in receiver in send and reply mode
+	/// </summary>
+	public class ExceptionResponse : IMessage
+	{
+		public string Text { get; set; } = string.Empty;
+	}
+}
